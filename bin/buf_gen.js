1163543e2aab9053a5c4b2942ca4c63a08162b4b
CC_MAC <chenchen_9694@outlook.com>
2024-09-09 11:48:11 +0800

    update scripts

--- bin/buf_gen.js
+++ bin/buf_gen.js
@@ -1,16 +1,59 @@
 #!/usr/bin/env node
 
 const { Command } = require('commander');
+const yaml = require('js-yaml');
+const fs = require('fs');
 const { execSync } = require('child_process');
 const program = new Command();
 
-// $ edubbo gen
+const bufGenYamlTemplate = {
+  version: 'v2',
+  clean: true,
+  plugins: [
+    {
+      local: 'node_modules/.bin/protoc-gen-es',
+      out: './dubbo/gen',
+      opt: [
+        'target=js',
+        'js_import_style=legacy_commonjs',
+      ],
+    },
+    {
+      local: 'node_modules/.bin/protoc-gen-dubbo3-cjs',
+      out: './dubbo/gen',
+      opt: [
+        'target=js',
+        'js_import_style=legacy_commonjs',
+      ],
+    },
+  ],
+  inputs: [
+    {
+      directory: './dubbo/proto/',
+    },
+  ],
+};
+
+// $ edubbo init 初始化，生产buf.gen.yaml文件
+program
+  .command('init')
+  .description('Generating buf.gen.yaml ...')
+  .action(() => {
+    try {
+      const yamlContent = yaml.dump(bufGenYamlTemplate);
+      fs.writeFileSync('./buf.gen.yaml', yamlContent);
+    } catch (err) {
+      console.error('Fail to generate:', err.message);
+    }
+  });
+
+// $ edubbo gen 根据proto定义文件生成cjs定义文件
 program
   .command('gen')
   .description('Generating cjs by proto-buf ...')
   .action(() => {
     try {
-      execSync('npm run buf-gen');
+      execSync('node node_modules/.bin/buf generate');
     } catch (err) {
       console.error('Fail to generate:', err.message);
     }
