a4c4627b42f2b14f337faa38bee8d6405a405893
CC_MAC <chenchen_9694@outlook.com>
2024-09-09 11:04:37 +0800

    fix scripts

--- bin/buf_gen.js
+++ bin/buf_gen.js
@@ -1,14 +1,19 @@
 #!/usr/bin/env node
 
-const { program } = require('commander');
+const { Command } = require('commander');
 const { execSync } = require('child_process');
+const program = new Command();
 
 // $ edubbo gen
 program
   .command('gen')
   .description('Generating cjs by proto-buf ...')
   .action(() => {
-    execSync('npm run buf-gen');
+    try {
+      execSync('npm run buf-gen');
+    } catch (err) {
+      console.error('Fail to generate:', err.message);
+    }
   });
 
 program.parse(process.argv);
